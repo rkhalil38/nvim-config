669e3add8321904bfbc9246cb3c36c67d74942af
rkhalil38 <romuluskhalil3@gmail.com>
2025-02-18 16:21:05 -0500

    migrated to lazy

---
+++ lua/romuluskhalil/config/lazy.lua
@@ -0,0 +1,60 @@
+ -- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    {'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {"williamboman/mason.nvim"},
+    {"lervag/vimtex", lazy = false, init = function() vim.g.vimtex_view_method = "zathura" end}, 
+    {'akinsho/toggleterm.nvim', version = "*", opts = {}},
+    {'theprimeagen/harpoon'},
+    {"catppuccin/nvim", name = "catppuccin", priority = 1000},
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        ---@module "ibl"
+        ---@type ibl.config
+        opts = {},
+    }
+
+
+
+
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true, notify = false},
+})
--- lua/romuluskhalil/init.lua
+++ lua/romuluskhalil/init.lua
@@ -1,12 +1,12 @@
+require("romuluskhalil.config.lazy")
 require("romuluskhalil.remap")
 require("romuluskhalil.set")
 
 function StatusWordCount()
-  local wc = vim.fn.wordcount()
-  if vim.fn.mode() == 'v' or vim.fn.mode() == 'V' or vim.fn.mode() == 'CTRL-V' then
-    return wc["visual_words"] or 0
-   else
-    return wc["words"]
-  end
+    local wc = vim.fn.wordcount()
+    if vim.fn.mode() == 'v' or vim.fn.mode() == 'V' or vim.fn.mode() == 'CTRL-V' then
+	return wc["visual_words"] or 0
+    else
+	return wc["words"]
+    end
 end
-
