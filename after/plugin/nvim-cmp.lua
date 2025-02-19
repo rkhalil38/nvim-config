5adc6525585ca569fdd2848bd23d28b42803fcbe
rkhalil38 <romuluskhalil3@gmail.com>
2025-02-19 10:00:12 -0500

    added more to cmp

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -12,8 +12,8 @@ cmp.setup({
       end,
     },
     window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -24,8 +24,9 @@ cmp.setup({
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
+      { name = 'path' },
       { name = 'luasnip' }, -- For luasnip users.
-    }, {
+      { name = 'treesitter' },
       { name = 'buffer' },
     })
   })
