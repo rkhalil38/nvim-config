cffc0b287af22ff6106586bc9a0d5c54703ab32a
rkhalil38 <romuluskhalil3@gmail.com>
2025-04-26 15:27:31 -0400

    added scroll off

--- lua/romuluskhalil/config/init.lua
+++ lua/romuluskhalil/config/init.lua
@@ -2,6 +2,9 @@ require("romuluskhalil.config.lazy")
 require("romuluskhalil.config.remap")
 require("romuluskhalil.config.set")
 
+-- set scroll off to 5
+vim.opt.scrolloff = 5
+
 function StatusWordCount()
     local wc = vim.fn.wordcount()
     if vim.fn.mode() == 'v' or vim.fn.mode() == 'V' or vim.fn.mode() == 'CTRL-V' then
