32ff6aaadd78d715d0ed4e9c117a29494b180fe8
rkhalil38 <romuluskhalil3@gmail.com>
2025-05-26 21:34:59 -0400

    updated lspconfig

--- lua/romuluskhalil/plugins/lsp.lua
+++ lua/romuluskhalil/plugins/lsp.lua
@@ -1,6 +1,6 @@
 return {
     {
-        "williamboman/mason.nvim",
+        "mason-org/mason-lspconfig.nvim",
         opts = {
             ensure_installed = {
                 "lua_ls",
@@ -15,30 +15,10 @@ return {
                 'marksman',
                 "clangd"
             }
-        }
-    },
-    { "williamboman/mason-lspconfig.nvim" },
-    {
-        dependencies = {
-            {
-                "folke/neoconf.nvim",
-                cmd = "Neoconf",
-                config = function()
-                    local lspconfig = require("lspconfig")
-                    local mason_lspconfig = require("mason-lspconfig")
-                    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-                    local capabilities = cmp_nvim_lsp.default_capabilities()
-
-                    mason_lspconfig.setup_handlers({
-                        function(server_name)
-                            lspconfig[server_name].setup({
-                                capabilities = capabilities,
-                            })
-                        end
-                    })
-                end
-            },
         },
-        "neovim/nvim-lspconfig",
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
     }
 }
