da5b3e46d2d8dd184216dc61d54967b01f5d591d
rkhalil38 <romuluskhalil3@gmail.com>
2025-06-16 13:10:24 -0400

    configured xdebug

--- lua/romuluskhalil/plugins/debugger.lua
+++ lua/romuluskhalil/plugins/debugger.lua
@@ -1,5 +1,24 @@
 return {
     {
         'mfussenegger/nvim-dap',
+        config = function ()
+            local dap = require('dap')
+
+            -- php debugger
+            dap.adapters.php = {
+                type = 'executable',
+                command = 'node',
+                args = { '/path/to/vscode-php-debug/out/phpDebug.js' }
+            }
+
+            dap.configurations.php = {
+                {
+                    type = 'php',
+                    request = 'launch',
+                    name = 'Listen for Xdebug',
+                    port = 9003
+                }
+            }
+        end
     }
 }
