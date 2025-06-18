88ee36ccf471993e0e610ccacfd6f7cc3c2c4ea2
rkhalil38 <romuluskhalil3@gmail.com>
2025-06-18 16:10:41 -0400

    added vue lsp and better ts_ls

--- lua/romuluskhalil/plugins/lsp.lua
+++ lua/romuluskhalil/plugins/lsp.lua
@@ -21,5 +21,26 @@ return {
             { "mason-org/mason.nvim", opts = {} },
             "neovim/nvim-lspconfig",
         },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function ()
+            vim.lsp.config('ts_ls', {
+                init_options = {
+                    plugins = {
+                        {
+                            name = "@vue/typescript-plugin",
+                            location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+                            languages = {"javascript", "typescript", "vue"},
+                        },
+                    },
+                },
+                filetypes = {
+                    "javascript",
+                    "typescript",
+                    "vue",
+                },
+            })
+        end
     }
 }
