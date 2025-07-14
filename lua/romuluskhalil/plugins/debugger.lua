87f7c8d3607de1bdc8cd98c880a8c4d5160e7864
rkhalil38 <romuluskhalil3@gmail.com>
2025-07-14 17:03:39 -0400

    debuggers

--- lua/romuluskhalil/plugins/debugger.lua
+++ lua/romuluskhalil/plugins/debugger.lua
@@ -8,17 +8,25 @@ return {
             dap.adapters.php = {
                 type = 'executable',
                 command = 'node',
-                args = { '~/.config/nvim/vscode-php-debug/out/phpDebug.js' }
+                args = { '/Users/romuluskhalil/.config/nvim/vscode-php-debug/out/phpDebug.js' }
             }
-
             dap.configurations.php = {
                 {
                     type = 'php',
                     request = 'launch',
                     name = 'Listen for Xdebug',
-                    port = 9003
+                    port = 9003,
+                    pathMappings = {
+                        ['/app'] = "${workspaceFolder}"
+                    },
+                    log = true
                 }
             }
+
+            vim.keymap.set("n", "<leader>bp", dap.toggle_breakpoint)
+            vim.keymap.set("n", "<leader>dc", dap.continue)
+            vim.keymap.set("n", "<leader>so", dap.step_over)
+            vim.keymap.set("n", "<leader>si", dap.step_into)
         end
     },
     { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
