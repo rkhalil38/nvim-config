d2207f5212191417f6dd690f775b4f6aed56413a
rkhalil38 <romuluskhalil3@gmail.com>
2026-04-01 15:08:23 -0400

    lua_lsp: now detects vim runtime

--- lua/romuluskhalil/plugins/lsp.lua
+++ lua/romuluskhalil/plugins/lsp.lua
@@ -32,6 +32,11 @@ require("mason-lspconfig").setup({
 --------------------------------------------------
 local config_dir = vim.fn.stdpath("config")
 
+-- IMPORTANT! --
+-- Note that nvim-lspconfig provides default configs for all lsps
+-- The changes below are merged with those defaults to form final config
+-- This means changes below are my own custom tweaks
+
 -- TypeScript / Vue setup
 vim.lsp.config("ts_ls", {
   init_options = {
@@ -58,5 +63,53 @@ vim.lsp.config("ruby_lsp", {
         enablePendingMigrationsPrompt = false,
       },
     },
+  },
+})
+
+-- Lua LSP setup
+vim.lsp.config('lua_ls', {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath('config')
+        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          -- Depending on the usage, you might want to add additional paths
+          -- here.
+          -- '${3rd}/luv/library',
+          -- '${3rd}/busted/library',
+        },
+        -- Or pull in all of 'runtimepath'.
+        -- NOTE: this is a lot slower and will cause issues when working on
+        -- your own configuration.
+        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+        -- library = vim.api.nvim_get_runtime_file('', true),
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
   },
 })
