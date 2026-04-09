34c4a50f77c75469ff6b815eb978ac85ad59ecd3
rkhalil38 <romuluskhalil3@gmail.com>
2026-04-09 17:23:11 -0400

    cleanup: removed toggleterm

--- lua/romuluskhalil/plugins/miscellaneous.lua
+++ lua/romuluskhalil/plugins/miscellaneous.lua
@@ -1,5 +1,4 @@
 vim.pack.add({
-    "https://github.com/akinsho/toggleterm.nvim",
     "https://github.com/iamcco/markdown-preview.nvim",
     "https://github.com/nvim-tree/nvim-web-devicons",
     "https://github.com/MeanderingProgrammer/render-markdown.nvim",
