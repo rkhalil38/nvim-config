01c224eccd2336400d5a6205c9b2ea44de6689e4
rkhalil38 <romuluskhalil3@gmail.com>
2026-05-13 23:20:51 -0400

    themes: fzf-lua theme search, cleaner theme config

--- lua/romuluskhalil/plugins/fzf-lua.lua
+++ lua/romuluskhalil/plugins/fzf-lua.lua
@@ -13,6 +13,7 @@ local fzf = require("fzf-lua")
 vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Find file (fzf-lua)" })
 vim.keymap.set("n", "<leader>fs", fzf.live_grep, { desc = "Find string (fzf-lua)" })
 vim.keymap.set("n", "<leader>fc", fzf.commands, { desc = "Find command (fzf-lua)" })
+vim.keymap.set("n", "<leader>ft", fzf.colorschemes, { desc = "Find theme (fzf-lua)" })
 vim.keymap.set("n", "<leader>gf", fzf.git_files, { desc = "Find git file (fzf-lua)" })
 vim.keymap.set("n", "<leader>gb", fzf.git_branches, { desc = "Find git branch (fzf-lua)" })
 
--- lua/romuluskhalil/plugins/theme.lua
+++ lua/romuluskhalil/plugins/theme.lua
@@ -1,4 +1,8 @@
 vim.pack.add({
     "https://github.com/ellisonleao/gruvbox.nvim",
     "https://github.com/jacoborus/tender.vim",
+    "https://github.com/EdenEast/nightfox.nvim"
 })
+
+-- setting colorscheme
+vim.cmd.colorscheme("nightfox")
