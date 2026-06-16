e1c9582a9e50f5a9045012da5a8c0bca0db10b81
rkhalil38 <romuluskhalil3@gmail.com>
2026-06-16 15:35:57 -0400

    plugin(avante): new ai setup

--- lua/romuluskhalil/plugins/ai.lua
+++ lua/romuluskhalil/plugins/ai.lua
@@ -1,14 +1,27 @@
 vim.pack.add({
+    "https://github.com/yetone/avante.nvim",
+    "https://github.com/nvim-lua/plenary.nvim",
+    "https://github.com/MunifTanjim/nui.nvim",
+    "https://github.com/HakonHarnes/img-clip.nvim",
+    "https://github.com/MeanderingProgrammer/render-markdown.nvim",
+    "https://github.com/hrsh7th/nvim-cmp",
+    "https://github.com/ibhagwan/fzf-lua",
+    "https://github.com/nvim-tree/nvim-web-devicons",
     "https://github.com/zbirenbaum/copilot.lua",
-    "https://github.com/CopilotC-Nvim/CopilotChat.nvim",
 })
 
 -----------------------------------------------
--- copilot.lua
+-- avante.nvim
 -----------------------------------------------
-require("copilot").setup()
 
------------------------------------------------
--- CopilotChat.nvim
------------------------------------------------
-require("CopilotChat").setup()
+-- Build
+local avante_path = vim.fn.stdpath("data") .. "/site/pack/core/opt/avante.nvim"
+if vim.fn.isdirectory(avante_path) == 1 then
+    local build_cmd = vim.fn.has("win32") ~= 0
+            and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        or "make"
+    vim.fn.jobstart({ "bash", "-c", "cd " .. avante_path .. " && " .. build_cmd })
+end
+
+-- Config
+require("avante").setup({})
